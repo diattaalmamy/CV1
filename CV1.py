0fe8d3923e0099c84b154005e907bb8cabab44f8
diattaalmamy <diattaalmamy22@gmail.com>
2026-02-16 20:52:47 +0000

    Add files via upload

---
+++ CV1.py
@@ -0,0 +1,40 @@
+import streamlit as st
+with st.sidebar:
+    st.title("📑 Mon Portfolium")
+    st.image("photo.jpg")
+    
+    st.markdown("***")
+    st.write("📞 CONTACTE")           
+    st.write("**Nom :** ALMAMY DIATTA")
+    st.write("**Telephone :** 789653625") 
+    st.write("**Email :** almamydiatta@gmail.com")
+    st.write("**Adresse :** Dakar,Senegal")
+
+    st.markdown("***")
+    st.write("📖 Parcoures scolaire")            
+    st.write("2026-2027:BTS")
+    st.write("2024-2025:BAC")
+
+    st.markdown("***")
+    st.write("Langues")
+    st.write("Français:Courant")
+    st.write("Anglais:Intermediaire")
+        
+st.header("🧑‍✈️ Profil")
+st.write("Géomaticien en formation motivé et passionné par le développement d'application en Python et Streamlit, avec un fort intéret pour les Systèmes D'information Géographique (SIG)")
+
+st.header("🎓 Formation")
+st.write("BTS en Géomatique")
+st.write("Centre d'Entreprenariat et de Développement Tecnique (G15)")
+st.write("Baccalauréat - Série L2")
+
+st.header("🧠 Compétences")
+
+st.write("Développment en Systèmes d'Information Géographique (SIG)")
+st.write("Notions de base en Carthographie")
+st.write("Gestion de bases de données Géographiques")
+st.write("Développement d'applications web avec Python et Streamlit")
+
+st.header(" 🔧 Expériences")
+
+st.write("Stage académique et projets personnels en SIG et développement Python")
